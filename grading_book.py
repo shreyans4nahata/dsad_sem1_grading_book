730b9b92be523446508b1f04b559a009c563317d
Abhishek <0426abhi@gmail.com>
2020-12-14 00:04:31 +0400

    Added inputPSFile and updated hash tables.

--- grading_book.py
+++ grading_book.py
@@ -1,40 +1,35 @@
 """
 This file contains the program for the grading book problem.
-Assigment Details : https://bits-pilani.instructure.com/courses/692/assignments/2846
+Assignment Details : https://bits-pilani.instructure.com/courses/692/assignments/2846
 
-This progrma has the following components:
+This program has the following components:
 1. Definition of hash function and creation of a hash table using primitive data structures
 2. Reading of input file and insertion into the hash table
 3. Reading prompt file and executing the prompts as:
   a. Creation of Hall of fame list
-  b. Creation of Lsit of students to be offered new courses
+  b. Creation of List of students to be offered new courses
   c. Creation of the Department Average CGPA list
 4. There will be a destroy hash function to be executed after the program ends or on exit
 """
 
 DEPT_LIST = ["CSE", "MEC", "ECE", "ARC"]
-HASHING_STRATER_SEED = 1000000
+HASHING_STARTER_SEED = 1000000
 HASH_TABLE_SIZE = 650000
 STARTING_YEAR = 2000
 STUDENT_ID_LENGTH = 11
 
-class customHash:
 
+class customHash:
     values_list = []
 
     def __init__(self):
-        """
-        Initialize the Empty hash table with the default size
-        """
-        self.values_list = [None]*HASH_TABLE_SIZE
+        self.values_list = [None] * HASH_TABLE_SIZE
 
-    def insert(self, key: str, value: float):
+    def insertStudentRec(self, current_index, key, value):
         """
         Inserts the record in the hash table if the provided key is valid.
         """
         try:
-            self._validate_input(key)
-            current_index = self.get_hash_key(key)
             if current_index >= HASH_TABLE_SIZE:
                 raise Exception("The entered key has hash generated more than table size", key, current_index)
             if self.values_list[current_index] is not None:
@@ -44,13 +39,14 @@ class customHash:
         except Exception as e:
             print("Error inserting the given key in the hash table.", key, e)
 
-    def _validate_input(self, key: str):
+    @staticmethod
+    def validate_input(key):
         """
         Key Validation based on the year and roll number format
         """
         if len(key) == STUDENT_ID_LENGTH:
             _a, _b, _c = int(key[:4]), str(key[4:7]), int(key[7:])
-            if _a <2010 or _a > 2016 or _b not in DEPT_LIST:
+            if _a < 2010 or _a > 2016 or _b not in DEPT_LIST:
                 raise Exception("The entered input key has incorrect year", key)
         else:
             raise Exception("The entered input is incorrect", key)
@@ -63,7 +59,7 @@ class customHash:
         Includes Input Validation to avoid unnecessary processing.
         """
         try:
-            self._validate_input(key)
+            self.validate_input(key)
             current_index = self.get_hash_key(key)
             if current_index >= HASH_TABLE_SIZE:
                 raise Exception("The entered key has hash generated more than table size", key)
@@ -71,10 +67,11 @@ class customHash:
                 return self.values_list[current_index][1]
             else:
                 return None
+
         except Exception as e:
             print("Error fetching the entry from the hash table.", key)
 
-    def get_hash_key(self, key: str):
+    def get_hash_key(self, key):
         """
         This function creates the unique hash id from the student Id given.
 
@@ -89,11 +86,11 @@ class customHash:
         do index correction by subtracting from the first record. (10 0 0000)
         """
         year, dept, roll = int(key[:4]), str(key[4:7]), int(key[7:])
-        year_offset = self._get_year_offset(year)
-        department_id = self._get_department_id(dept)
-        return int(str(year_offset) + str(department_id) + str(roll)) - HASHING_STRATER_SEED
+        year_offset = self.get_year_offset(year)
+        department_id = self.get_department_id(dept)
+        return int(str(year_offset) + str(department_id) + str(roll)) - HASHING_STARTER_SEED
 
-    def _get_year_offset(self, year: int):
+    def get_year_offset(self, year):
         """
         As the year of study/courses start from 2010,
         we can convert the years into its difference from the starting year (i.e. 2000).
@@ -102,7 +99,8 @@ class customHash:
         """
         return year - STARTING_YEAR
 
-    def _get_department_id(self, dept: str):
+    @staticmethod
+    def get_department_id(dept):
         """
         We can assign a unique number to each course.
         This function return the index of the current branch from the list of
@@ -110,21 +108,37 @@ class customHash:
         """
         return DEPT_LIST.index(dept)
 
+    def getStudentRecords(self):
+        """
+        Read the inputPS18.txt file.
+        This function will update the student record from text file to hash table.
+        """
+        try:
+            f = open("inputPS18.txt", "r")
+            if f.mode == 'r':
+                read_file = f.readlines()
+                for record in read_file:
+                    split_data = record.split("/")
+                    # It will generate the hash key and save the value.
+                    self.validate_input(split_data[0])
+                    current_index = self.get_hash_key(split_data[0])
+                    self.insertStudentRec(current_index, split_data[0], split_data[1])
+        except Exception as e:
+            print("Failed to read input file.")
 
 
-"""
 # customHash Test
 cHash = customHash()
-cHash.insert("2010CSE1234", 4.5)
-cHash.insert("2000CSE1234", 4.5)
-cHash.insert("2014MEC1231", 4.5)
-cHash.insert("2016ARC1234", 4.5)
-cHash.insert("20CSE1234", 4.5)
-cHash.insert("20a0CSE1234", 4.5)
-print(cHash.get_key("2010CSE1234"))
-print(cHash.get_key("2000CSE1234"))
-print(cHash.get_key("2014MEC1231"))
-print(cHash.get_key("2016ARC1234"))
-print(cHash.get_key("20CSE1234"))
-print(cHash.get_key("2010C3E1234"))
-"""
+cHash.getStudentRecords()
+# cHash.insert("2010CSE1234", 4.5)
+# # cHash.insert("2000CSE1234", 4.5)
+# cHash.insert("2014MEC1231", 4.5)
+# cHash.insert("2016ARC1234", 4.5)
+# # cHash.insert("20CSE1234", 4.5)
+# # cHash.insert("20a0CSE1234", 4.5)
+print(cHash.get_key("2010CSE1223"))
+# # print(cHash.get_key("2000CSE1234"))
+# print(cHash.get_key("2014MEC1231"))
+# print(cHash.get_key("2016ARC1234"))
+# # print(cHash.get_key("20CSE1234"))
+# # print(cHash.get_key("2010C3E1234"))
