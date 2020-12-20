b8145a29e41fc8a2b1feaa67ade3c34144e4605d
Shreyansh <shreyans4nahata@gmail.com>
2020-12-20 13:40:06 +0530

    Changes in the current year hardcoding and hash table gerneation. Rectified logic. Added the design

--- README.md
+++ README.md
@@ -18,14 +18,33 @@ Assignment 1 – PS18 - [Grade Book] : https://bits-pilani.instructure.com/cours
 ### Requirements
 1. Create an input file input.txt with a random list of students per year and their corresponding CGPA (maximum of 5.0 point CGPA).
  a. For students that have graduated, the CGPA would be the overall CGPA and for those who are still to complete the program, you can consider their CGPA to be a temporary CGPA.
+ b. In the year 2010 the first batch of 20 students were admitted to the university. Now in the year 2020, 200 students were admitted across all departments. So the starting year for the university is 2010.
 2. Design a hash table, which uses student Id as the key to hash elements into the hash table. Generate necessary hash table definitions needed.
 3. Design a hash function HashId() which accepts the student-ID as a parameter and returns the hash value. You are only allowed to use basic arithmetic and logic operations in implementing this hash function.
 
 ### Solution
 #### Approach
+
+We use the lazy approach to generate the hash table. 
+This ensures that all our saves are mapped to unique keys and there are no conflicts during insertion.
+Given, we have a very short span of the operational years of the college (2010 - 2020) and limited departments (4), our approach can easily be implemented by using more space and lesser time.
+ 
 Since the hash function to be generated is for a given student Id, we can leverage the student Id attributes for the purpose of creating the hash Id. The student Ids in themselves are unique so creating another hash Id is a redundant process as we can directly create a map with the student ids as keys.
 Student Id consists of Year, Department and Roll number. We can assign a unique number to each course.
+
 As the year of study/courses start from 2010, we can convert the years into its difference from the starting year (i.e. 2010). So 2020 can be converted into 2020-2010 = 10.
 
 Based on the above, the hash value can be generated as a concatenation of
 year_offset, branch Id and roll number.
+
+Therefore, the person who joined CSE in 2010 and was given a roll number of 0001 would have a hash value of :
+year_offset = 2010 - 2010
+branch_id (CSE) : 0
+roll: 0001
+key : 000001 -> 1
+
+Similarly, we can map each of these keys. The total max size of our hash table would be 640K.
+
+**Design:**
+
+![IMG](https://drive.google.com/uc?export=view&id=1S_skSnGEucSgqoaUazqjQVR4F7E9NnmH)
--- grading_book.py
+++ grading_book.py
@@ -12,11 +12,18 @@ This program has the following components:
 4. There will be a destroy hash function to be executed after the program ends or on exit
 """
 import ast
+import datetime
+
+current_time = datetime.datetime.now()
 
 DEPT_LIST = ["CSE", "MEC", "ECE", "ARC"]
-HASH_TABLE_SIZE = 650000
+
+# This is the year university started as per requirements
 STARTING_YEAR = 2010
-CURRENT_YEAR = 2020
+
+CURRENT_YEAR = current_time.year
+HASH_TABLE_SIZE = int(str(CURRENT_YEAR - STARTING_YEAR - 4) + str(len(DEPT_LIST)-1) + "9999")
+
 STUDENT_ID_LENGTH = 11
 INPUT_PS = "inputPS18.txt"
 PROMPTS_PS = "promptsPS18.txt"
@@ -33,6 +40,8 @@ class customHash:
     def insert_student_record(self, current_index, key, value):
         """
         Inserts the record in the hash table if the provided key is valid.
+
+        Complexity : O(1)
         """
         try:
             if current_index >= HASH_TABLE_SIZE:
@@ -47,6 +56,8 @@ class customHash:
     def _validate_input(self, key):
         """
         Key Validation based on the year and roll number format
+
+        Complexity: O(1)
         """
         if len(key) == STUDENT_ID_LENGTH:
             _a, _b, _c = int(key[:4]), str(key[4:7]), int(key[7:])
@@ -61,6 +72,8 @@ class customHash:
         If the data for the given key is not present the function returns None
 
         Includes Input Validation to avoid unnecessary processing.
+
+        Complexity : O(1)
         """
         try:
             self._validate_input(key)
@@ -86,8 +99,9 @@ class customHash:
         Student Id consists of Year, Department and Roll number.
 
         Based on the above, the hash value can be generated as a concatenation of
-        year_offset, branch Id and roll number and to get the correct index we can
-        do index correction by subtracting from the first record. (10 0 0000)
+        year_offset, branch Id and roll number.
+
+        Complexity: O(1)
         """
         year, dept, roll = int(key[:4]), str(key[4:7]), int(key[7:])
         year_offset = self._get_year_offset(year)
@@ -100,6 +114,8 @@ class customHash:
         we can convert the years into its difference from the starting year (i.e. 2000).
 
         This function returns the offset of the current year from the Starting year.
+
+        Complexity: O(1)
         """
         return year - STARTING_YEAR
 
@@ -108,6 +124,8 @@ class customHash:
         We can assign a unique number to each course.
         This function return the index of the current branch from the list of
         pre defined branches.
+
+        Complexity: O(1)
         """
         return DEPT_LIST.index(dept)
 
@@ -115,6 +133,8 @@ class customHash:
         """
         This function reads the INPUT_PS file and
         updates the student record from text file to hash table.
+
+        Complexity: O(n) , where n is the number of records in the file
         """
         try:
             with open(INPUT_PS, "r") as input_file:
@@ -140,6 +160,8 @@ class customHash:
         """
         In prompt file tags are mentioned according to that steps will be method will be
         executed.
+
+        Complexity: O(n) , as the underlying operations have complexity of O(n) and the given function runs in O(1)
         """
         try:
             with open(PROMPTS_PS, "r") as prompt_file:
@@ -159,9 +181,13 @@ class customHash:
         """
         This function prints the list of all students who have graduated
         and  topped  their  department  in  their year of graduation
+
+        Complexity: O(n) , n being the number of records in the table.
+        The other 2 nested for loops iterate over constant sizes and not variable sizes
+        so the complexity from them is O(1)
         """
         max_cgpa_per_branch = [[[None, 0] for _ in range(len(DEPT_LIST))]
-                               for __ in range(CURRENT_YEAR - STARTING_YEAR - 4)]
+                               for __ in range(CURRENT_YEAR - STARTING_YEAR - 3)]
         with open(OUTPUT_PS, "w") as output_file:
             output_file.truncate(0)
             output_file.write("---------- hall of fame ----------")
@@ -171,9 +197,9 @@ class customHash:
                     year, dept = int(roll[:4]), roll[4:7]
                     cgpa = float(cgpa)
                     if year <= (CURRENT_YEAR - 4):
-                        if max_cgpa_per_branch[year - STARTING_YEAR - 1][self._get_department_id(dept)][1] < cgpa:
-                            max_cgpa_per_branch[year - STARTING_YEAR - 1][self._get_department_id(dept)][0] = roll
-                            max_cgpa_per_branch[year - STARTING_YEAR - 1][self._get_department_id(dept)][1] = cgpa
+                        if max_cgpa_per_branch[year - STARTING_YEAR][self._get_department_id(dept)][1] < cgpa:
+                            max_cgpa_per_branch[year - STARTING_YEAR][self._get_department_id(dept)][0] = roll
+                            max_cgpa_per_branch[year - STARTING_YEAR][self._get_department_id(dept)][1] = cgpa
             total_toppers = 0
             for i in range(CURRENT_YEAR - STARTING_YEAR - 4):
                 for j in range(len(DEPT_LIST)):
@@ -189,6 +215,13 @@ class customHash:
             output_file.write(SECTION_END_SEPARATOR)
 
     def new_course_list(self, record):
+        """
+        This function outputs the eligible students who have graduated in the last 5 years and whose CGPA fall under the
+        CGPA range passed as part of the record
+
+        Complexity: O(n) , n being the number of records in the table.
+        :param record: containing the starting and ending CGPAs
+        """
         cgpa_range = record.split(":")
         NEW_COURSE_YEARS = self._get_eligible_years()
         eligible_students = []
@@ -208,6 +241,8 @@ class customHash:
         """
         This function evaluates the max and average CGPA for each branch
         and saves the output to a file.
+
+        Complexity: O(n) , n being the number of records in the table.
         """
         try:
             dept_count = len(DEPT_LIST)
@@ -238,34 +273,33 @@ class customHash:
     def destroy_hash(self):
         """
         This function just empties the list of our records.
+
+        Complexity: O(1)
         """
         self.values_list = []
 
     @staticmethod
     def _get_eligible_years():
         """
+        As we need to calculate people graduating in the last 5 years so a person who joined 9 years back would have
+        graduated in the last 5th year and the person who joined 4 years back would have graduated last year.
+        The students who joined 3 years back are yet t graduate this year. So our upper bound is Current year - 3
+        Similarly, the students who joined in current year - 5 - 3 would have graduated 5 years back, so our lower bound
+        is current year - 8.
+
+        Complexity: O(1) , as the year count is a constant value
+
         :return: list of years eligible for new course list operation
         """
         __l = []
-        for __i in range(CURRENT_YEAR- 5, CURRENT_YEAR - 3):
+
+        for __i in range(CURRENT_YEAR - 8, CURRENT_YEAR - 3):
             __l.append(__i)
         return __l
+
 
 # customHash Test
 cHash = customHash()
 cHash.populate_student_table()
 cHash.read_prompts()
 cHash.destroy_hash()
-# cHash.read_prompts()
-# cHash.insert("2010CSE1234", 4.5)
-# # cHash.insert("2000CSE1234", 4.5)
-# cHash.insert("2014MEC1231", 4.5)
-# cHash.insert("2016ARC1234", 4.5)
-# # cHash.insert("20CSE1234", 4.5)
-# # cHash.insert("20a0CSE1234", 4.5)
-# print(cHash.get_key("2010CSE1223"))
-# # print(cHash.get_key("2000CSE1234"))
-# print(cHash.get_key("2014MEC1231"))
-# print(cHash.get_key("2016ARC1234"))
-# # print(cHash.get_key("20CSE1234"))
-# # print(cHash.get_key("2010C3E1234"))
